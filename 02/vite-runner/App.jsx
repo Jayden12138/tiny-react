eec179e73a3307153938697feb28b8b997ba41d4
Jayden12138 <18993882884@163.com>
2024-03-23 20:08:19 +0800

    使用jsx

---
+++ 02/vite-runner/App.jsx
@@ -0,0 +1,12 @@
+import React from './core/React.js'
+
+// const App = React.createElement(
+// 	'div',
+// 	{ id: 'root' },
+// 	'hello world! ',
+// 	'Jayden'
+// )
+const App = <div id="root">hello world! Jayden</div>
+console.log(App)
+
+export default App
