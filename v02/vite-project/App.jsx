5c6383fba6b251454dafbda7830cce1064f80e4b
Jayden12138 <18993882884@163.com>
2024-03-25 16:28:11 +0800

    fiber

--- v02/vite-project/App.jsx
+++ v02/vite-project/App.jsx
@@ -2,6 +2,17 @@
 import CReact from './core/React.js'
 
 // const App = React.createElement('div', { id: 'app' }, 'hello world')
-const App = <div id="app">hello world</div>
+const App = (
+	<div id="app">
+		<div className="child child-1">
+			<div className="child child-1-1">child 1-1</div>
+			<div className="child child-1-2">child 1-2</div>
+		</div>
+		<div className="child child-2">
+			<div className="child child-2-1">child 2-1</div>
+			<div className="child child-2-2">child 2-2</div>
+		</div>
+	</div>
+)
 
 export default App
--- v02/vite-project/core/React.js
+++ v02/vite-project/core/React.js
@@ -20,29 +20,103 @@ function createElement(type, props, ...children) {
 	}
 }
 
-function render(el, container) {
+function createDom(type) {
 	// dom
-	const dom =
-		el.type === 'TEXT_ELEMENT'
-			? document.createTextNode('')
-			: document.createElement(el.type)
+	return type === 'TEXT_ELEMENT'
+		? document.createTextNode('')
+		: document.createElement(type)
+}
 
+function updateProps(dom, props) {
 	// props
-	Object.keys(el.props).forEach(key => {
+	Object.keys(props).forEach(key => {
 		if (key !== 'children') {
-			dom[key] = el.props[key]
+			dom[key] = props[key]
 		}
 	})
+}
 
-	// children
-	const children = el.props.children
-	if (children) {
-		children.forEach(child => render(child, dom))
+function initChildren(work) {
+	console.log(work, work.props.children)
+	const children = work.props.children
+	let prevChild = null
+	children.forEach((child, index) => {
+		const newWork = {
+			type: child.type,
+			props: child.props,
+			dom: null,
+			parent: work,
+			child: null,
+			sibling: null,
+		}
+		if (index === 0) {
+			work.child = newWork
+		} else {
+			prevChild.sibling = newWork
+		}
+		prevChild = newWork
+	})
+}
+
+//
+function render(el, container) {
+	// root 主入口
+	nextWork = {
+		dom: container,
+		props: {
+			children: [el],
+		},
+	}
+}
+
+let nextWork = null
+function workLoop(deadline) {
+	let shouldYield = false
+
+	while (!shouldYield && nextWork) {
+		nextWork = performWorkOfUnit(nextWork)
+
+		shouldYield = deadline.timeRemaining() < 1
 	}
 
-	// append
-	container.append(dom)
+	requestIdleCallback(workLoop)
 }
+
+// 执行任务
+function performWorkOfUnit(work) {
+	// 1. 创建dom
+	// 2. props
+	// 3. 转换链表 设置好指针
+	// 4. 返回下一个要执行的任务
+
+	console.log(work)
+
+	if (!work.dom) {
+		// 创建dom
+		const dom = (work.dom = createDom(work.type))
+		work.parent.dom.append(dom)
+
+		// props
+		updateProps(dom, work.props)
+	}
+
+	// children
+	initChildren(work)
+
+	// 返回下一个任务
+	if (work.child) {
+		return work.child
+	}
+
+	let nextWork = work
+	while (nextWork) {
+		if (nextWork.sibling) {
+			return nextWork.sibling
+		}
+		nextWork = nextWork.parent
+	}
+}
+requestIdleCallback(workLoop)
 
 export default {
 	createElement,
