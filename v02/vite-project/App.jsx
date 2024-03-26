1e797c5e36493274f95a3a5a73d2f7d64a7e1e11
Jayden12138 <18993882884@163.com>
2024-03-26 18:00:56 +0800

    update props

--- v02/vite-project/App.jsx
+++ v02/vite-project/App.jsx
@@ -4,36 +4,24 @@ import CReact from './core/React.js'
 // const App = React.createElement('div', { id: 'app' }, 'hello world')
 
 // function component
-function Counter({ num }) {
+let count = 10
+function Counter() {
 	function handleClick() {
-		console.log('click')
+		count++
+		CReact.update()
 	}
 	return (
 		<div>
-			<span>Counter: {num}</span>
+			<span>Counter: {count}</span>
 			<button onClick={handleClick}>click</button>
 		</div>
 	)
 }
 
-function CountContianer() {
-	return <Counter num={30} />
-}
-
-// const App = (
-// 	<div id="app">
-// 		<Counter num={10} />
-// 		<Counter num={20} />
-// 		<CountContianer />
-// 	</div>
-// )
-
 function App() {
 	return (
 		<div id="app">
-			<Counter num={10} />
-			{/* <Counter num={20} />
-			<CountContianer /> */}
+			<Counter />
 		</div>
 	)
 }
